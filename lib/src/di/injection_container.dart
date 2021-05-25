f3968c3f967e49e699e49c088373b161f2ceabe9
Muhammad Noerhidayatullah <mnoer@outlook.com>
2021-05-25 19:37:32 +0700

    fix authrepository injection

--- lib/src/di/injection_container.dart
+++ lib/src/di/injection_container.dart
@@ -1,9 +1,10 @@
 import 'package:get_it/get_it.dart';
 import 'package:school_data_module/src/repositories/auth_repository_impl.dart';
+import 'package:school_domain_module/school_domain_module.dart';
 
 final inject = GetIt.instance;
 
 Future<void> init() async {
   // Repository
-  inject.registerFactory(() => AuthRepositoryImpl(inject()));
+  inject.registerFactory<AuthRepository>(() => AuthRepositoryImpl(inject()));
 }
