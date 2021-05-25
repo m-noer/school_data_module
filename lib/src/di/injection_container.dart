a9614918fd44b6a95685216ddb6bd3ab19826c18
Muhammad Noerhidayatullah <mnoer@outlook.com>
2021-05-25 19:43:50 +0700

    add remotedatasource injection

--- lib/src/di/injection_container.dart
+++ lib/src/di/injection_container.dart
@@ -1,4 +1,5 @@
 import 'package:get_it/get_it.dart';
+import 'package:school_data_module/school_data_module.dart';
 import 'package:school_data_module/src/repositories/auth_repository_impl.dart';
 import 'package:school_domain_module/school_domain_module.dart';
 
@@ -7,4 +8,8 @@ final inject = GetIt.instance;
 Future<void> init() async {
   // Repository
   inject.registerFactory<AuthRepository>(() => AuthRepositoryImpl(inject()));
+
+  // Datasources
+  inject
+      .registerFactory<RemoteDataSource>(() => RemoteDataSourceImpl(inject()));
 }
